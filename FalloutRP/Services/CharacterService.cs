51d5635d4611960a8e8da698c9a33210153165e4
Derwinia <deniscolin777@yahoo.fr>
2023-12-14 17:24:25 +0100

    correct character update

--- FalloutRP/Services/CharacterService.cs
+++ FalloutRP/Services/CharacterService.cs
@@ -14,259 +14,6 @@ namespace FalloutRP.Services
         {
             _falloutRPContext = falloutRPContext;
         }
-        //public CharacterDTO? CharacterGetById(int id)
-        //{
-        //    Character character = _falloutRPContext.Characters
-        //        .Include(a => a.Attributes)
-        //        .Include(s => s.Skill)
-        //        .Include(b => b.BodyParts)
-        //        .Include(r => r.Reputations)
-        //        .Include(w => w.Weapons)
-        //        .Include(p => p.Perks)
-        //        .Include(i => i.Inventory)
-        //        .ThenInclude(a => a.Ammos)
-        //        .Include(i => i.Inventory)
-        //        .ThenInclude(c => c.Chemicals)
-        //        .Include(i => i.Inventory)
-        //        .ThenInclude(d => d.Drinks)
-        //        .Include(i => i.Inventory)
-        //        .ThenInclude(e => e.Equipements)
-        //        .Include(i => i.Inventory)
-        //        .ThenInclude(f => f.Foods)
-        //        .Include(i => i.Inventory)
-        //        .ThenInclude(m => m.Materials)
-        //        .FirstOrDefault(c => c.Id == id);
-
-        //    if (character == null)
-        //    {
-        //        throw new KeyNotFoundException("Cet utilisateur n'existe pas");
-        //    }
-
-        //    List<BodyPartDTO> bodyParts = new List<BodyPartDTO>();
-            
-        //    foreach (BodyPart bodypart in character.BodyParts)
-        //    {
-        //        BodyPartDTO bp = new BodyPartDTO
-        //        {
-        //            Part = bodypart.Part,
-        //            PhysicalResilience = bodypart.PhysicalResilience,
-        //            RadiationResilience = bodypart.RadiationResilience,
-        //            EnergyResilience = bodypart.EnergyResilience,
-        //            HealthResilience = bodypart.HealthResilience,
-        //        };
-        //        bodyParts.Add(bp);
-        //    }
-
-        //    List<ReputationDTO> reputations = new List<ReputationDTO>();
-
-        //    foreach (Reputation reputation in character.Reputations)
-        //    {
-        //        ReputationDTO rep = new ReputationDTO
-        //        {
-        //            Name = reputation.Name,
-        //            Rank = reputation.Rank
-        //        };
-        //        reputations.Add(rep);
-        //    }
-
-        //    List<WeaponDTO> weapons = new List<WeaponDTO>();
-
-        //    foreach (Weapon weapon in character.Weapons)
-        //    {
-        //        WeaponDTO weap = new WeaponDTO
-        //        {
-        //            Name = weapon.Name,
-        //            TN = weapon.TN,
-        //            DC = weapon.DC,
-        //            PhysicalDamage = weapon.PhysicalDamage,
-        //            EnergyDamage = weapon.EnergyDamage,
-        //            RadiationDamage = weapon.RadiationDamage,
-        //            PoisonDamage = weapon.PoisonDamage,
-        //            Effects = weapon.Effects,
-        //            Proprieties = weapon.Proprieties,
-        //            RateOfFire = weapon.RateOfFire,
-        //            Range = weapon.Range,
-        //            Ammo = weapon.Ammo,
-        //            Weigth = weapon.Weigth,
-        //        };
-        //        weapons.Add(weap);
-        //    }
-
-        //    List<PerkDTO> perks = new List<PerkDTO>();
-
-        //    foreach (Perk perk in character.Perks)
-        //    {
-        //        PerkDTO pk = new PerkDTO
-        //        {
-        //            Name = perk.Name,
-        //            Rank = perk.Rank,
-        //            Effect = perk.Effect,
-        //        };
-        //        perks.Add(pk);
-        //    }
-
-        //    List<AmmoDTO> ammos = new List<AmmoDTO>();
-
-        //    foreach (Ammo ammo in character.Inventory.Ammos)
-        //    {
-        //        AmmoDTO am = new AmmoDTO
-        //        {
-        //            Name = ammo.Name,
-        //            Quantity = ammo.Quantity,
-        //            Weight = ammo.Weight,
-        //        };
-        //        ammos.Add(am);
-        //    }
-
-        //    List<ChemicalDTO> chemicals = new List<ChemicalDTO>();
-
-        //    foreach (Chemical chemical in character.Inventory.Chemicals)
-        //    {
-        //        ChemicalDTO chem = new ChemicalDTO
-        //        {
-        //            Name = chemical.Name,
-        //            Quantity = chemical.Quantity,
-        //            Weight = chemical.Weight,
-        //        };
-        //        chemicals.Add(chem);
-        //    }
-
-        //    List<DrinkDTO> drinks = new List<DrinkDTO>();
-
-        //    foreach (Drink drink in character.Inventory.Drinks)
-        //    {
-        //        DrinkDTO drk = new DrinkDTO
-        //        {
-        //            Name = drink.Name,
-        //            Quantity = drink.Quantity,
-        //            Weight = drink.Weight,
-        //        };
-        //        drinks.Add(drk);
-        //    }
-
-        //    List<EquipementDTO> equipements = new List<EquipementDTO>();
-
-        //    foreach (Equipement equipement in character.Inventory.Equipements)
-        //    {
-        //        EquipementDTO equip = new EquipementDTO
-        //        {
-        //            Name = equipement.Name,
-        //            Quantity = equipement.Quantity,
-        //            Weight = equipement.Weight,
-        //        };
-        //        equipements.Add(equip);
-        //    }
-
-        //    List<FoodDTO> foods = new List<FoodDTO>();
-
-        //    foreach (Food food in character.Inventory.Foods)
-        //    {
-        //        FoodDTO fd = new FoodDTO
-        //        {
-        //            Name = food.Name,
-        //            Quantity = food.Quantity,
-        //            Weight = food.Weight,
-        //        };
-        //        foods.Add(fd);
-        //    }
-
-        //    List<MaterialDTO> materials = new List<MaterialDTO>();
-
-        //    foreach (Material material in character.Inventory.Materials)
-        //    {
-        //        MaterialDTO mat = new MaterialDTO
-        //        {
-        //            Name = material.Name,
-        //            Quantity = material.Quantity,
-        //            Weight = material.Weight,
-        //        };
-        //        materials.Add(mat);
-        //    }
-
-        //    CharacterDTO characterDTO = new CharacterDTO()
-        //    {
-        //        Name = character.Name,
-        //        Xp = character.Xp,
-        //        XpToNext = character.XpToNext,
-        //        Origin = character.Origin,
-        //        Level = character.Level,
-        //        MeleeBonus = character.MeleeBonus,
-        //        Defence = character.Defence,
-        //        Initiative = character.Initiative,
-        //        HealthPoint = character.HealthPoint,
-        //        HealthPointMax = character.HealthPointMax,
-        //        MentalHealthPoint = character.MentalHealthPoint,
-        //        MentalHealthPointMax = character.MentalHealthPointMax,
-        //        PoisonResilience = character.PoisonResilience,
-        //        Background = character.Background,
-        //        Caps = character.Caps,
-        //        MaxWeight = character.MaxWeight,
-        //        Attributes = new AttributeDTO
-        //        {
-        //            Strength = character.Attributes.Strength,
-        //            Perception = character.Attributes.Perception,
-        //            Endurance = character.Attributes.Endurance,
-        //            Charisme = character.Attributes.Charisme,
-        //            Intelligence = character.Attributes.Intelligence,
-        //            Agility = character.Attributes.Agility,
-        //            Luck = character.Attributes.Luck,
-        //            LuckPoints = character.Attributes.LuckPoints,
-        //        },
-        //        Skills = new SkillDTO {
-        //            RightHanded = character.Skill.RightHanded,
-        //            LeftHanded = character.Skill.LeftHanded,
-        //            Athletics = character.Skill.Athletics,
-        //            Lockpicking = character.Skill.Lockpicking,
-        //            Lockpickinglvl = 0,
-        //            Speech = character.Skill.Speech,
-        //            Speechlvl = 0,
-        //            Stealth = character.Skill.Stealth,
-        //            Stealthlvl = 0,
-        //            Medecine = character.Skill.Medecine,
-        //            Medecinelvl = 0,
-        //            Driving = character.Skill.Driving,
-        //            Drivinglvl = 0,
-        //            Repair = character.Skill.Repair,
-        //            Repairlvl = 0,
-        //            Science = character.Skill.Science,
-        //            Sciencelvl = 0,
-        //            Survival = character.Skill.Survival,
-        //            Survivallvl = 0,
-        //            Bartering = character.Skill.Bartering,
-        //            Barteringlvl = 0,
-        //            BareHands = character.Skill.BareHands,
-        //            BareHandslvl = 0,
-        //            MeleeWeapon = character.Skill.MeleeWeapon,
-        //            MeleeWeaponlvl = 0,
-        //            LightWeapon = character.Skill.LightWeapon,
-        //            LightWeaponlvl = 0,
-        //            HeavyWeapon = character.Skill.HeavyWeapon,
-        //            HeavyWeaponlvl = 0,
-        //            EnergieWeapon = character.Skill.EnergieWeapon,
-        //            EnergieWeaponlvl = 0,
-        //            ThrowingWeapon = character.Skill.ThrowingWeapon,
-        //            ThrowingWeaponlvl = 0,
-        //            Explosive = character.Skill.Explosive,
-        //            Explosivelvl = 0,
-        //            Game = character.Skill.Game,
-        //            Gamelvl = 0,
-        //        },
-        //        BodyParts = bodyParts,
-        //        Reputations = reputations,
-        //        Weapons = weapons,
-        //        Perks = perks,
-        //        Inventories = new InventoryDTO
-        //        {
-        //            Ammos = ammos,
-        //            Chemicals = chemicals,
-        //            Drinks = drinks,
-        //            Equipements = equipements,
-        //            Foods = foods,
-        //            Materials = materials,
-        //        }
-        //    };
-        //    return characterDTO;
-        //}
 
         public CharacterDTO? CharacterGetByPseudo(string pseudo)
         {
@@ -530,7 +277,7 @@ namespace FalloutRP.Services
                 Perks = perks,
                 Inventories = new InventoryDTO
                 {
-                    Id = character.Id,
+                    Id = character.Inventory.Id,
                     Ammos = ammos,
                     Chemicals = chemicals,
                     Drinks = drinks,
@@ -569,7 +316,7 @@ namespace FalloutRP.Services
             {
                 throw new KeyNotFoundException("Ce personnage n'existe pas");
             }
-
+            #region character
             character.Name = characterUpdate.Name;
             character.Xp = characterUpdate.Xp;
             character.XpToNext = characterUpdate.XpToNext;
@@ -586,7 +333,9 @@ namespace FalloutRP.Services
             character.Background= characterUpdate.Background;
             character.Caps= characterUpdate.Caps;
             character.MaxWeight= characterUpdate.MaxWeight;
+            #endregion
 
+            #region attributes
             character.Attributes.Strength = characterUpdate.Attributes.Strength;
             character.Attributes.Perception= characterUpdate.Attributes.Perception;
             character.Attributes.Endurance= characterUpdate.Attributes.Endurance;
@@ -595,7 +344,9 @@ namespace FalloutRP.Services
             character.Attributes.Agility= characterUpdate.Attributes.Agility;
             character.Attributes.Luck= characterUpdate.Attributes.Luck;
             character.Attributes.LuckPoints= characterUpdate.Attributes.LuckPoints;
+            #endregion
 
+            #region skills
             character.Skill.RightHanded = characterUpdate.Skills.RightHanded;
             character.Skill.LeftHanded = characterUpdate.Skills.LeftHanded;
             character.Skill.Athletics = characterUpdate.Skills.Athletics;
@@ -634,7 +385,121 @@ namespace FalloutRP.Services
             character.Skill.Explosivelvl = characterUpdate.Skills.Explosivelvl;
             character.Skill.Game = characterUpdate.Skills.Game;
             character.Skill.Gamelvl = characterUpdate.Skills.Gamelvl;
+            #endregion
 
+            #region bodypart
+            foreach (BodyPart bodypart in character.BodyParts)
+            {
+                BodyPartDTO bp = characterUpdate.BodyParts.Find(b => b.Id == bodypart.Id);
+                if(bp != null)
+                {
+                    bodypart.PhysicalResilience = bp.PhysicalResilience;
+                    bodypart.RadiationResilience = bp.RadiationResilience;
+                    bodypart.EnergyResilience = bp.EnergyResilience;
+                    bodypart.HealthResilience = bp.HealthResilience;
+                }
+            }
+            
+            #endregion
+
+            #region reputation
+            foreach (Reputation rep in character.Reputations)
+            {
+                ReputationDTO newrep = characterUpdate.Reputations.Find(r => r.Id == rep.Id);
+                rep.Rank = newrep.Rank;
+                rep.Name = newrep.Name;
+            }
+            #endregion
+
+            #region weapon
+            foreach (Weapon weapon in character.Weapons)
+            {
+                WeaponDTO newweapon = characterUpdate.Weapons.Find(w => w.Id == weapon.Id);
+                weapon.Name = newweapon.Name;
+                weapon.TN = newweapon.TN;
+                weapon.DC = newweapon.DC;
+                weapon.PhysicalDamage = newweapon.PhysicalDamage;
+                weapon.EnergyDamage = newweapon.EnergyDamage;
+                weapon.RadiationDamage = newweapon.RadiationDamage;
+                weapon.PoisonDamage = newweapon.PoisonDamage;
+                weapon.Effects = newweapon.Effects;
+                weapon.Proprieties = newweapon.Proprieties;
+                weapon.RateOfFire = newweapon.RateOfFire;
+                weapon.Range = newweapon.Range;
+                weapon.Ammo = newweapon.Ammo;
+                weapon.Weigth = newweapon.Weigth;
+            }
+            #endregion
+
+            #region perks
+            foreach (Perk perk in character.Perks)
+            {
+                PerkDTO newperk = characterUpdate.Perks.Find(r => r.Id == perk.Id);
+                perk.Name = newperk.Name;
+                perk.Rank = newperk.Rank;
+                perk.Effect = newperk.Effect;
+            }
+            #endregion
+
+            #region ammo
+            foreach (Ammo ammo in character.Inventory.Ammos)
+            {
+                AmmoDTO newammo = characterUpdate.Inventories.Ammos.Find(r => r.Id == ammo.Id);
+                ammo.Name = newammo.Name;
+                ammo.Quantity = newammo.Quantity;
+                ammo.Weight = newammo.Weight;
+            }
+            #endregion
+
+            #region chemical
+            foreach (Chemical chemical in character.Inventory.Chemicals)
+            {
+                ChemicalDTO newchem = characterUpdate.Inventories.Chemicals.Find(r => r.Id == chemical.Id);
+                chemical.Name = newchem.Name;
+                chemical.Quantity = newchem.Quantity;
+                chemical.Weight = newchem.Weight;
+            }
+            #endregion
+
+            #region drink
+            foreach (Drink drink in character.Inventory.Drinks)
+            {
+                DrinkDTO newdrink = characterUpdate.Inventories.Drinks.Find(r => r.Id == drink.Id);
+                drink.Name = newdrink.Name;
+                drink.Quantity = newdrink.Quantity;
+                drink.Weight = newdrink.Weight;
+            }
+            #endregion
+
+            #region equipement
+            foreach (Equipement equip in character.Inventory.Equipements)
+            {
+                EquipementDTO newequip = characterUpdate.Inventories.Equipements.Find(r => r.Id == equip.Id);
+                equip.Name = newequip.Name;
+                equip.Quantity = newequip.Quantity;
+                equip.Weight = newequip.Weight;
+            }
+            #endregion
+
+            #region food
+            foreach (Food food in character.Inventory.Foods)
+            {
+                FoodDTO newfood = characterUpdate.Inventories.Foods.Find(r => r.Id == food.Id);
+                food.Name = newfood.Name;
+                food.Quantity = newfood.Quantity;
+                food.Weight = newfood.Weight;
+            }
+            #endregion
+
+            #region material
+            foreach (Material mat in character.Inventory.Materials)
+            {
+                MaterialDTO newmat = characterUpdate.Inventories.Materials.Find(r => r.Id == mat.Id);
+                mat.Name = newmat.Name;
+                mat.Quantity = newmat.Quantity;
+                mat.Weight = newmat.Weight;
+            }
+            #endregion
 
             _falloutRPContext.SaveChanges();
         }
